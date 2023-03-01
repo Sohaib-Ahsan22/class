ae08a192f1d6deb2ddc8be16086a9a3f9e9060ac
Sohaib Ahsan <125086696+Sohaib-Ahsan22@users.noreply.github.com>
2023-03-01 23:51:23 +0500

    Add files via upload

---
+++ new 10.py
@@ -0,0 +1,7 @@
+print("Question:05")
+import math
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+hcf=math.gcd(num1,num2)
+print (hcf)
+
---
+++ new q8.py
@@ -0,0 +1,3 @@
+print ("question no.7")
+datalist=[1452,11.23,1+2j,true,'w3resource',(0,-1),[5,12],{"class":'v',"section":'A'}]
+for i in (datalist):
