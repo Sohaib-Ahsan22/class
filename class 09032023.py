9d889d517f3d6e985b2081396b2e6d34e2ee33be
Sohaib Ahsan <125086696+Sohaib-Ahsan22@users.noreply.github.com>
2023-03-09 15:35:42 +0500

    Add files via upload

---
+++ class 09032023.py
@@ -0,0 +1,7 @@
+x=int(input("enter max number of stars:"))
+for i in range(1,x+1):
+    for j in range(i):
+        right(print("*",end=" "))
+    print ()
+    
+        
---
+++ right side triangle.py
@@ -0,0 +1,6 @@
+x=int(input("enter max number of stars:"))
+for i in range(x,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print ()
+        
